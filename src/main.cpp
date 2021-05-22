b46f59c35767fb2309bdf089c5393c1b3f541017
Mirko Rüther <mirkoruether@users.noreply.github.com>
2021-05-22 18:35:08 +0200

    Init platformio project

---
+++ src/main.cpp
@@ -0,0 +1,92 @@
+#include <Arduino.h>
+#include <M5StickCPlus.h>
+#include <OneButton.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+#include "secret.h"
+
+#define CONNECT_TIMEOUT_MS 30000
+
+float accX = 0.0F;
+float accY = 0.0F;
+float accZ = 0.0F;
+
+float gyroX = 0.0F;
+float gyroY = 0.0F;
+float gyroZ = 0.0F;
+
+float pitch = 0.0F;
+float roll  = 0.0F;
+float yaw   = 0.0F;
+
+// connectToWiFi adapted from ESP32 example code. See, e.g.:
+// https://github.com/espressif/arduino-esp32/blob/master/libraries/WiFi/examples/WiFiClient/WiFiClient.ino
+void connectToWiFi() {
+    unsigned long startTime = millis();
+    Serial.println("Connecting to: " + String(WIFI_SSID));
+
+    WiFi.disconnect();
+    WiFi.begin(WIFI_SSID, WIFI_PWD);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+
+        if (millis() - startTime > CONNECT_TIMEOUT_MS) {
+            Serial.println();
+            Serial.println("Failed to connect.");
+            return;
+        }
+    }
+
+    WiFi.setAutoReconnect(true);
+
+    Serial.println();
+    Serial.println("Connected.");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void setup() {
+    Serial.begin(9600);
+    M5.begin();
+    M5.IMU.Init();
+
+    connectToWiFi();
+
+    M5.Lcd.setRotation(3);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setTextSize(1);
+    M5.Lcd.setCursor(80, 15);
+    M5.Lcd.println("IMU TEST");
+    M5.Lcd.setCursor(30, 30);
+    M5.Lcd.println("  X       Y       Z");
+    M5.Lcd.setCursor(30, 70);
+    M5.Lcd.println("  Pitch   Roll    Yaw");
+}
+
+float temp = 0;
+
+void loop() {
+    M5.IMU.getGyroData(&gyroX,&gyroY,&gyroZ);
+    M5.IMU.getAccelData(&accX,&accY,&accZ);
+    M5.IMU.getAhrsData(&pitch,&roll,&yaw);
+    M5.IMU.getTempData(&temp);
+    
+    M5.Lcd.setCursor(30, 40);
+    M5.Lcd.printf("%6.2f  %6.2f  %6.2f      ", gyroX, gyroY, gyroZ);
+    M5.Lcd.setCursor(170, 40);
+    M5.Lcd.print("o/s");
+    M5.Lcd.setCursor(30, 50);
+    M5.Lcd.printf(" %5.2f   %5.2f   %5.2f   ", accX, accY, accZ);
+    M5.Lcd.setCursor(170, 50);
+    M5.Lcd.print("G");
+    M5.Lcd.setCursor(30, 80);
+    M5.Lcd.printf(" %5.2f   %5.2f   %5.2f   ", pitch, roll, yaw);
+
+    M5.Lcd.setCursor(30, 95);
+    M5.Lcd.printf("Temperature : %.2f C", temp);
+    delay(100);
+}
